f00c8b7d0bdee88fce7fd75a576d4c0b6939b356
01Ander <arinconayala@hotmail.com>
2025-11-12 17:48:00 -0500

    logic adjustment

--- models/accounts.py
+++ models/accounts.py
@@ -28,11 +28,13 @@ class BankAccount:
 
 
 class CreditCard:
-    def __init__(self, name=None, limit=0.0, current_debt=0.0, due_day=None, bank_account=None):
+    def __init__(self, bank_account, name=None, limit=0.0, current_debt=0.0, due_day=None):
         self.name = name
         self.limit = limit
         self.current_debt = current_debt
         self.due_day = due_day
+        if bank_account is None:
+            raise ValueError('Credit Card requires a linked Bank Account')
         self.bank_account = bank_account
 
     def purchase(self, amount):
@@ -46,36 +48,40 @@ class CreditCard:
     def pay(self, amount):
         if amount <= 0:
             raise ValueError('Pay amount must be positive')
-        self.bank_account.balance -= amount
+        # self.bank_account.balance -= amount
+        if amount > self.current_debt:
+            raise ValueError('Impossible to pay more than the debt')
+        self.bank_account.withdraw(amount)
         self.current_debt -= amount
 
 
-my_account = BankAccount('Andersson')
-print(f'Create bank account with balance: {my_account.balance}')
-my_account.deposit(1750)
-print(f'Deposit done, new balance: {my_account.balance}')
+# my_account = BankAccount('Andersson')
+# print(f'Create bank account with balance: {my_account.balance}')
+# my_account.deposit(1750)
+# print(f'Deposit done, new balance: {my_account.balance}')
 
-my_credit_card = CreditCard('Mastercard', 8000, 0, bank_account=my_account)
-print(
-    f'Create credit card with current debit: {my_credit_card.current_debt}, and limit: {my_credit_card.limit}')
-my_credit_card.purchase(1500)
-print(
-    f'Purchase 1 approve: new debit: {my_credit_card.current_debt}')
-my_credit_card.pay(1500)
-print(
-    f'Pay done. New debit: {my_credit_card.current_debt}. Balance account: {my_account.balance}')
-# my_credit_card.purchase(8500)
+# my_credit_card = CreditCard(my_account, 'Mastercard', 8000, 0)
 # print(
-#     # fail successfully
-#     f'Purchase 2 approve: new debit: {my_credit_card.current_debt}')
-my_credit_card.purchase(7500)
-print(
-    f'Purchase 3 approve: new debit: {my_credit_card.current_debt}')
+#     f'Create credit card with current debit: {my_credit_card.current_debt}, and limit: {my_credit_card.limit}')
+# my_credit_card.purchase(1500)
+# print(
+#     f'Purchase 1 approve: new debit: {my_credit_card.current_debt}')
+# my_credit_card.pay(1500)
+# print(
+#     f'Pay done. New debit: {my_credit_card.current_debt}. Balance account: {my_account.balance}')
+# # my_credit_card.purchase(8500)
+# # print(
+# #     # fail successfully
+# #     f'Purchase 2 approve: new debit: {my_credit_card.current_debt}')
+# my_credit_card.purchase(7500)
+# print(
+#     f'Purchase 3 approve: new debit: {my_credit_card.current_debt}')
+# # my_credit_card.pay(7500)
+# # print(
+# #     # fail successfully
+# #     f'Pay done. New debit: {my_credit_card.current_debt}, New balance: {my_account}')
+# my_account.deposit(10000)
+# print(f'Deposit 2 done, new balance: {my_account.balance}')
 # my_credit_card.pay(7500)
 # print(
-#     f'Pay done. New debit: {my_credit_card.current_debt}, New balance: {my_account}') # fail successfully
-my_account.deposit(10000)
-print(f'Deposit 2 done, new balance: {my_account.balance}')
-my_credit_card.pay(7500)
-print(
-    f'Pay 2 done. New debit: {my_credit_card.current_debt}, New balance: {my_account.balance}')
+#     f'Pay 2 done. New debit: {my_credit_card.current_debt}, New balance: {my_account.balance}')
