870698b0ca8768ada3a285d2a107b0efd12d300f
01Ander <arinconayala@hotmail.com>
2025-11-13 18:19:49 -0500

    implemented apply method

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-# from models.accounts import BankAccount, CreditCard
-# from models.transactions import Transaction, RecurringTransaction
+from models.accounts import BankAccount, CreditCard
+from models.transactions import Transaction
+
+
+my_account = BankAccount('Andersson')
+print(f'Create bank account with balance: {my_account.balance}')
+my_credit_card = CreditCard(my_account, 'Mastercard', 8000, 0)
+print(
+    f'Create credit card with current debit: {my_credit_card.current_debt}, and limit: {my_credit_card.limit}')
+
+transaction1 = Transaction('13/11/2025', 15000, 'Salary', 'income', my_account)
+transaction1.apply()
+print(f'Transaction 1 done. New balance: {my_account.balance}')
+transaction2 = Transaction(
+    '14/11/2025', 200, 'Netflix', 'expense', my_credit_card)
+transaction2.apply()
+print(f'Transaction 2 done. New debit: {my_credit_card.current_debt}')
+transaction3 = Transaction('14/11/2025', 1200, 'Shoes', 'expense', my_account)
+transaction3.apply()
+print(f'Transaction 3 done. New balance :{my_account.balance}')
+transaction4 = Transaction(
+    '14/11/2025', 1200, 'Shoes', 'expense', my_credit_card)
+transaction4.apply()
+print(f'Transaction 4 done. New debit :{my_credit_card.current_debt}')
+transaction5 = Transaction(
+    '25/11/2025', 1400, 'Pay credit card', 'pay_credit', my_credit_card)
+transaction5.apply()
+print(
+    f'Transaction 5 done. New debit :{my_credit_card.current_debt} & new balance: {my_account.balance}')
--- models/transactions.py
+++ models/transactions.py
@@ -4,11 +4,11 @@ from models.accounts import BankAccount, CreditCard
 class Transaction:
     def __init__(self, date, amount, description, trans_type, target):
         self.date = date
-        if (amount < 0):
+        if (amount <= 0):
             raise ValueError('Amount must be more than zero')
         self.amount = amount
         self.description = description
-        if (trans_type != 'Income' and trans_type != 'Expense'):
+        if (trans_type != 'income' and trans_type != 'expense' and trans_type != 'pay_credit'):
             raise ValueError('Please select a valid transaction type')
         self.trans_type = trans_type
         if (not isinstance(target, BankAccount) and not isinstance(target, CreditCard)):
@@ -16,4 +16,17 @@ class Transaction:
         self.target = target
 
     def apply(self):
-        if self.trans_type is 'Income' and self.target is BankAccount:
+        if self.trans_type == 'income' and isinstance(self.target, BankAccount):
+            self.target.deposit(self.amount)
+        elif self.trans_type == 'expense':
+            if isinstance(self.target, BankAccount):
+                self.target.withdraw(self.amount)
+            if isinstance(self.target, CreditCard):
+                self.target.purchase(self.amount)
+        elif self.trans_type == 'pay_credit':
+            if not isinstance(self.target, CreditCard):
+                raise ValueError(
+                    "pay_credit transactions require a CreditCard target")
+            self.target.pay(self.amount)
+        else:
+            raise ValueError('Transaction invalid')
